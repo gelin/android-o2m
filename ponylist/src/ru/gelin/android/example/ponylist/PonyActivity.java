bf310b1225ce28d605efa1591a403cfdeeebd555
Denis Nelubin <dnelubin@gmail.com>
2014-07-15 23:08:40 +0700

    Added Rarity and Applejack images.

--- ponylist/src/ru/gelin/android/example/ponylist/PonyActivity.java
+++ ponylist/src/ru/gelin/android/example/ponylist/PonyActivity.java
@@ -26,7 +26,7 @@ public class PonyActivity extends Activity {
 
     Fragment getFragment() {
         if (this.fragment == null) {
-            this.fragment = PonyFragment.newInstance(5);    //TODO get param
+            this.fragment = PonyFragment.newInstance(1);    //TODO get param
         }
         return this.fragment;
     }
