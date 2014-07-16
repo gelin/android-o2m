5638b6fc2d7db3ea6f34ce793a13e46d49aa52a7
Denis Nelubin <dnelubin@gmail.com>
2014-07-16 23:13:04 +0700

    Added highlight of selected list item.

--- ponylist/src/ru/gelin/android/example/ponylist/MainListFragment.java
+++ ponylist/src/ru/gelin/android/example/ponylist/MainListFragment.java
@@ -7,7 +7,11 @@ import android.support.v4.app.ListFragment;
 import android.view.*;
 import android.widget.*;
 
-public class MainListFragment extends ListFragment implements AdapterView.OnItemClickListener {
+public class MainListFragment extends ListFragment {
+
+    static final String SELECTED_ITEM_KEY = "selectedItem";
+    static final int SELECTED_ITEM_DEFAULT = 0;
+    int selectedItem = -1;
 
     public static MainListFragment newInstance() {
         return new MainListFragment();
@@ -22,26 +26,39 @@ public class MainListFragment extends ListFragment implements AdapterView.OnItem
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         setListAdapter(new MainListAdapter(getActivity()));
-        ListView list = getListView();
-        list.setOnItemClickListener(this);
+        ListView listView = getListView();
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+        if (getResources().getBoolean(R.bool.dual_pane)) {
+            if (savedInstanceState != null) {
+                onListItemClick(getListView(), savedInstanceState.getInt(SELECTED_ITEM_KEY, SELECTED_ITEM_DEFAULT));
+            } else {
+                onListItemClick(getListView(), SELECTED_ITEM_DEFAULT);
+            }
+        }
     }
 
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-        onListItemClick((ListView)adapterView, position);
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SELECTED_ITEM_KEY, this.selectedItem);
+    }
+
+    @Override
+    public void onListItemClick(ListView listView, View view, int position, long id) {
+        onListItemClick(listView, position);
     }
 
     void onListItemClick(ListView listView, int position) {
         if (getResources().getBoolean(R.bool.dual_pane)) {
-//            if (this.selectedItem == position) {
-//                return;
-//            }
+            if (this.selectedItem == position) {
+                return;
+            }
             listView.setItemChecked(position, true);
             showFragment(position);
         } else {
             startActivity(position);
         }
-//        this.selectedItem = position;
+        this.selectedItem = position;
     }
 
     void showFragment(int index) {
